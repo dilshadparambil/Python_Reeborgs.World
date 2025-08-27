b0f2c37a5f2376de272c0599789c39c2df973064
Dilshad P <dilshadkareemparambil@gmail.com>
2025-08-27 09:16:14 +0530

    added game 15

---
+++ solutions/15_harvest_3/harvest3.py
@@ -0,0 +1,35 @@
+def enter_plot():
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def harvest_row():
+    for i in range(6):
+        while object_here():
+            take()
+        put()
+        move()
+        
+        
+    else:
+        if is_facing_north():
+            turn_right()
+            move()
+            turn_right()
+            move()
+        else:
+            turn_left()
+            move()
+            turn_left()
+            move()
+
+enter_plot()
+for i in range(6):
+    harvest_row()
