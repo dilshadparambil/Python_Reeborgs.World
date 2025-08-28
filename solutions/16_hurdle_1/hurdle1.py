e53cf491047c1424f0bcabbb233ce7b284fd6e4c
Dilshad P <dilshadkareemparambil@gmail.com>
2025-08-28 06:22:43 +0530

    added game 16 files

---
+++ solutions/16_hurdle_1/hurdle1.py
@@ -0,0 +1,16 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while not at_goal():
+    if wall_in_front() and wall_on_right():
+        turn_left()
+    elif wall_on_right():
+        move() 
+    else:
+        turn_right()
+        move()
+        turn_right()
+        move()
+        
